62bef50e5214e73e2c592c4b105be4747d5dce83
pinchas33 <rozenbergpinchas@gmail.com>
2019-11-10 19:02:31 +0200

    add valuse

---
+++ dotNet5780_01_7583_0258/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_01_7583_0258
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
