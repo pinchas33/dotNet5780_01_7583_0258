694450151cf9ad0b59065d5f80552af6712d7d31
pinchas33 <rozenbergpinchas@gmail.com>
2019-11-12 20:49:24 +0200

    created a third array with the gap between the other 2 arrays

--- dotNet5780_01_7583_0258/Program.cs
+++ dotNet5780_01_7583_0258/Program.cs
@@ -8,16 +8,27 @@ namespace dotNet5780_01_7583_0258
 {
     class Program
     {
+        
+
         static void Main(string[] arg)
         {
             Random rand = new random();
             int[] A = new int[20]; 
             int[] B = new int[20]; 
-            for (int i = 0; i < 20; i++)
+            int[] C = new int[20]; 
+
+            for (int i = 0; i < A.Length(); i++)
             {
                 A[i] = rand.Next(18, 123);
                 B[i] = rand.Next(18, 123);
             }
+
+            for (int i = 0; i < C.Length(); i++)
+            {
+                C[i] = Math.Abs(A[i] - B[i]);
+            }
+
+            Console.WriteLine();
 
         }
     }
