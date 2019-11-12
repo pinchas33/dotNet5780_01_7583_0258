f39e8adfc15e1a6642ccaf8da5c8c9f58a5d19c9
pinchas33 <rozenbergpinchas@gmail.com>
2019-11-12 20:43:43 +0200

    initilized 2 arrays with random numbers

--- dotNet5780_01_7583_0258/Program.cs
+++ dotNet5780_01_7583_0258/Program.cs
@@ -8,9 +8,17 @@ namespace dotNet5780_01_7583_0258
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] arg)
         {
-            int c = 7;
+            Random rand = new random();
+            int[] A = new int[20]; 
+            int[] B = new int[20]; 
+            for (int i = 0; i < 20; i++)
+            {
+                A[i] = rand.Next(18, 123);
+                B[i] = rand.Next(18, 123);
+            }
+
         }
     }
 }
